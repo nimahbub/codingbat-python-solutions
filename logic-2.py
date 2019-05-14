e402feb694853e7c8f9bccda1f78358f5416db2e
Nosin Ibna Mahbub <nosin.pic@gmail.com>
2019-05-15 03:15:32 +0600

    Create logic-2.py

---
+++ logic-2.py
@@ -0,0 +1,15 @@
+"""
+We want to make a row of bricks that is goal inches long.
+We have a number of small bricks (1 inch each) and big bricks (5 inches each).
+Return True if it is possible to make the goal by choosing from the given bricks.
+This is a little harder than it looks and can be done without any loops
+
+"""
+def make_bricks(small, big, goal):
+  if goal > small + big * 5:
+    return False
+  else:
+    if (goal % 5 <= small):
+      return True
+    return False
+
