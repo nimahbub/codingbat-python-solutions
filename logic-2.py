fcfb5bbf021cf73d2bb6a36bbf86c6d4ea1c7bc8
Nosin Ibna Mahbub <nosin.pic@gmail.com>
2019-05-15 04:05:05 +0600

    Update logic-2.py

--- logic-2.py
+++ logic-2.py
@@ -34,6 +34,25 @@ This is a little harder than it looks and can be done without any loops
       return a
     else:
       return a+b+c
+    
+    
+    
+    ##=============================================================================================>>>
+    def lucky_sum(a, b, c):
+          """   
+          Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count
+          towards the sum and values to its right do not count. So for example, 
+          if b is 13, then both b and c do not count.
+          """
+      if (a !=13 and b!=13 and c!=13):
+          return a+b+c
+      if(a==13):
+          return 0
+      if (b==13):
+          return a
+      if (c==13):
+        return a+b
+
 
 
   
